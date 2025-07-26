273000e708a08656024e66ce7ce4b6d4a74510b1
jakawan <duyduy2521@gmail.com>
2025-07-26 21:47:19 +0700

    Add alias @ for absolute imports in Vercel"

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  webpack(config) {
+    config.resolve.alias['@'] = __dirname;
+    return config;
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+module.exports = nextConfig;
