af97ea0d1697b561486c10a513c6983dec291c7f
jakkawan10 <duyduy2521@gmail.com>
2025-07-30 09:04:13 +0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+
 import type React from "react"
 
 import { useState, useEffect } from "react"
