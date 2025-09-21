9ed58910a3f875af84e49e43dad4b7af50b39170
jakawan <duyduy2521@gmail.com>
2025-09-21 17:44:27 +0700

    แก้lib/useUserRole.ts

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,7 +1,8 @@
 'use client'
 
-import { useAuth } from '@/lib/useAuth'
-import { useUserRole } from '@/lib/useUserRole'
+import { useAuth } from '@/lib/hooks/useAuth'
+import { useUserRole } from '@/lib/hooks/useUserRole'
+
 
 export function useUser() {
   const { user, loading: authLoading } = useAuth()
